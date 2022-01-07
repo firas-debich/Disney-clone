b5e64e50abebd363573d769fb9fe20f9af0c1b77
firas-debich <debichf02@gmail.com>
2022-01-07 17:50:17 +0100

    start working on login components

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,16 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Login from "./components/Login"
 function App() {
   return (
-    <div >
-          hello world 
-    </div>
+    <Router>
+
+    <Routes>
+      <Route path="/" exact element={<Login/>} />
+
+    </Routes>
+
+  </Router>
   );
 }
 
